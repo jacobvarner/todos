2a9e2bee4d3143a4d2f1cc9bca7db7cc434860a5
Jacob <jacob.varner14@gmail.com>
2016-01-20 18:04:26 -0600

    Added ability to add items to the list, display the items, and then also delete items from the list.

--- todos.js
+++ todos.js
@@ -1,10 +1,40 @@
-if(Meteor.isClient{
+Todos = new Mongo.Collection('todos');
+
+if(Meteor.isClient){
   // Client code goes here
+  Template.todos.helpers({
+    'todo': function(){
+      return Todos.find({}, {sort: {createdAt: -1}});
+    }
+  });
+
+  Template.addTodo.events({
+    'submit form': function(event){
+      event.preventDefault();
+      var todoName = $('[name="todoName"]').val();
+      Todos.insert({
+        name: todoName,
+        completed: false,
+        createdAt: new Date()
+      });
+      $('[name="todoName"]').val('');
+    }
+  });
+
+  Template.todoItem.events({
+    'click .delete-todo': function(event){
+      event.preventDefault();
+      var documentId = this._id;
+      var confirm = window.confirm("Delete this task?");
+      if(confirm){
+        Todos.remove({ _id: documentId });
+      }
+    }
+  });
 
 }
 
 if(Meteor.isServer){
   // Server code goes here
-  
-}
 
+}
