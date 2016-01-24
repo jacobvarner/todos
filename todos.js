2c9defdf33fa26aae2457b13d5ba5311e37f9f7b
Jacob <jacob.varner14@gmail.com>
2016-01-24 13:56:40 -0600

    Added ability for users to Register, Login, and Logout.

--- todos.js
+++ todos.js
@@ -117,6 +117,36 @@ if(Meteor.isClient){
     }
   });
 
+  Template.register.events({
+    'submit form': function(event){
+      event.preventDefault();
+      var email = $('[name=email]').val();
+      var password = $('[name=password]').val();
+      Accounts.createUser({
+        email: email,
+        password: password
+      });
+      Router.go('home');
+    }
+  });
+
+  Template.login.events({
+    'submit form': function(event){
+      event.preventDefault();
+      var email = $('[name=email]').val();
+      var password = $('[name=password]').val();
+      Meteor.loginWithPassword(email, password);
+    }
+  })
+
+  Template.navigation.events({
+    'click .logout': function(event){
+      event.preventDefault();
+      Meteor.logout();
+      Router.go('login');
+    }
+  });
+
 }
 
 if(Meteor.isServer){
