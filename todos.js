de5072db5c116a743c7822823f21544880972ec9
Jacob <jacob.varner14@gmail.com>
2016-03-06 22:57:27 -0600

    Added validation using a useful jQuery plugin that was also packaged for Meteor. Now the register and login forms are validated and more informative to the user when errors arise.

--- todos.js
+++ todos.js
@@ -33,6 +33,88 @@ Router.route('/list/:_id', {
 });
 
 if(Meteor.isClient){
+  $.validator.setDefaults({
+    rules: {
+        email: {
+            required: true,
+            email: true
+        },
+        password: {
+            required: true,
+            minlength: 6
+        }
+    },
+    messages: {
+        email: {
+            required: "You must enter an email address.",
+            email: "You've entered an invalid email address."
+        },
+        password: {
+            required: "You must enter a password.",
+            minlength: "Your password must be at least {0} characters."
+        }
+    }
+});
+
+  Template.login.onCreated(function(){
+    console.log("The 'login' template was just created.");
+  });
+
+  Template.login.onRendered(function(){
+    var validator = $('.login').validate({
+      submitHandler: function(event){
+        var email = $('[name=email]').val();
+        var password = $('[name=password]').val();
+        Meteor.loginWithPassword(email, password, function(error){
+          if(error){
+            if(error.reason == "User not found"){
+              validator.showErrors({
+                email: "That email doesn't belong to a registered user."
+              });
+            }
+            if(error.reason == "Incorrect password"){
+              validator.showErrors({
+                password: "You entered an incorrect password."
+              });
+            }
+          } else {
+            var currentRoute = Router.current().route.getName();
+            if (currentRoute == "login") {
+              Router.go('home');
+            }
+          }
+        });
+      }
+    });
+  });
+
+  Template.register.onRendered(function(){
+    var validator = $('.register').validate({
+      submitHandler: function(event){
+        var email = $('[name=email]').val();
+        var password = $('[name=password]').val();
+        Accounts.createUser({
+          email: email,
+          password: password
+        }, function(error){
+          if(error){
+            if(error.reason == "Email already exists."){
+              validator.showErrors({
+                email: "That email already belongs to a registered user."
+              });
+            }
+          } else {
+            Router.go("home");
+          }
+        });
+      }
+    });
+  });
+
+  Template.login.onDestroyed(function(){
+    console.log("The 'login' template was just destroyed.");
+  });
+
   // Client code goes here
   Template.todos.helpers({
     'todo': function(){
@@ -135,7 +217,7 @@ if(Meteor.isClient){
   Template.register.events({
     'submit form': function(event){
       event.preventDefault();
-      var email = $('[name=email]').val();
+      /*var email = $('[name=email]').val();
       var password = $('[name=password]').val();
       Accounts.createUser({
         email: email,
@@ -146,25 +228,13 @@ if(Meteor.isClient){
         } else {
           Router.go('home');
         }
-      });
+      });*/
     }
   });
 
   Template.login.events({
     'submit form': function(event){
       event.preventDefault();
-      var email = $('[name=email]').val();
-      var password = $('[name=password]').val();
-      Meteor.loginWithPassword(email, password, function(error){
-        if(error){
-          console.log(error.reason);
-        } else {
-          var currentRoute = Router.current().route.getName();
-          if (currentRoute == "login") {
-            Router.go('home');
-          }
-        }
-      });
     }
   })
 
